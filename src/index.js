e3a668f762f3479085162ec1111a3ee0001a484f
Josana-Kla <josana.0205@gmail.com>
2022-09-09 15:35:08 -0300

    databse: created connection with mongo db

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import express from 'express';
-import { MongoClient } from "mongodb";
-import dotenv from 'dotenv';
 import cors from 'cors';
 import joi from 'joi';
-
-dotenv.config();
 
 const app = express();
 app.use(cors());
